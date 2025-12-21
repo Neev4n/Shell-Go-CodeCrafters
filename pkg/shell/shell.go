378ca480052908582a4882e2427eea107347bd8d
Naveen Kannangara <asszincrd@gmail.com>
2025-12-22 10:52:16 +1100

    refactored shell code for readability and better design

--- pkg/shell/shell.go
+++ pkg/shell/shell.go
@@ -18,21 +18,29 @@ var ErrMissingRedirectDestination = errors.New("missing redirect destination")
 // type Builtin
 type Builtin func(args []string, s *Shell) error
 
-type RedirectBindings struct {
-	prevOut io.Writer
-	file    *os.File
+type redirectType int
+
+const (
+	in redirectType = iota
+	out
+	err
+)
+
+type Redirection struct {
+	redirectType redirectType
+	flag         int
 }
 
 // type Shell
 type Shell struct {
-	in              *bufio.Reader
-	Out             io.Writer
-	Err             io.Writer
-	pathDirs        []string
-	builtins        map[string]Builtin
-	executor        Executor
-	parser          Parser
-	redirectBinding RedirectBindings
+	in           *bufio.Reader
+	Out          io.Writer
+	Err          io.Writer
+	pathDirs     []string
+	builtins     map[string]Builtin
+	redirections map[string]Redirection
+	executor     Executor
+	parser       Parser
 }
 
 // func New
@@ -44,37 +52,29 @@ func New(reader io.Reader, out, errw io.Writer) *Shell {
 		dirs = strings.Split(path, string(os.PathListSeparator))
 	}
 
-	s := &Shell{
+	shell := &Shell{
 		in:       bufio.NewReader(reader),
 		Out:      out,
 		Err:      errw,
 		pathDirs: dirs,
 		builtins: make(map[string]Builtin),
-		redirectBinding: RedirectBindings{
-			prevOut: out,
-		},
 	}
 
-	s.executor = &DefaultExecutuor{LookupFunc: s.Lookup}
-	s.parser = NewDefaultParser()
-	s.registerBuiltins()
-	return s
+	shell.executor = &DefaultExecutuor{LookupFunc: shell.Lookup}
+	shell.parser = NewDefaultParser()
+	shell.registerBuiltins()
+	shell.registerRedirections()
+	return shell
 }
 
 //func Run
 
-func (s *Shell) Run() error {
+func (shell *Shell) Run() error {
 	for {
 
-		if s.redirectBinding.file != nil {
-			s.Out = s.redirectBinding.prevOut
-			s.redirectBinding.file.Close()
-			s.redirectBinding.file = nil
-		}
+		fmt.Fprint(shell.Out, "$ ")
 
-		fmt.Fprint(s.Out, "$ ")
-
-		line, err := s.in.ReadString('\n')
+		line, err := shell.in.ReadString('\n')
 
 		if err != nil {
 			return err
@@ -85,19 +85,19 @@ func (s *Shell) Run() error {
 			continue
 		}
 
-		fields, err := s.parser.Parse(line)
+		parsedArgs, err := shell.parser.Parse(line)
 
 		if err != nil {
 			return err
 		}
 
-		cmd := fields[0]
+		command := parsedArgs[0]
 		args := []string{}
-		if len(fields) > 1 {
-			args = fields[1:]
+		if len(parsedArgs) > 1 {
+			args = parsedArgs[1:]
 		}
 
-		cleanArgs, ioBindings, cleanup, ok := s.prepareIOforRedirection(args)
+		cleanArgs, ioBindings, cleanup, ok := shell.prepareIOforRedirection(args)
 
 		if !ok {
 			continue
@@ -108,23 +108,23 @@ func (s *Shell) Run() error {
 		}
 
 		// check built ins
-		if fn, ok := s.builtins[cmd]; ok {
+		if builtinFunc, ok := shell.builtins[command]; ok {
 
-			prevErr := s.Err
-			prevOut := s.Out
+			prevErr := shell.Err
+			prevOut := shell.Out
 
 			if ioBindings.Stderr != nil {
-				s.Err = ioBindings.Stderr
+				shell.Err = ioBindings.Stderr
 			}
 
 			if ioBindings.Stdout != nil {
-				s.Out = ioBindings.Stdout
+				shell.Out = ioBindings.Stdout
 			}
 
-			if err := fn(cleanArgs, s); err != nil {
+			if err := builtinFunc(cleanArgs, shell); err != nil {
 
-				s.Out = prevOut
-				s.Err = prevErr
+				shell.Out = prevOut
+				shell.Err = prevErr
 
 				if errors.Is(err, ErrExit) {
 
@@ -135,10 +135,10 @@ func (s *Shell) Run() error {
 					return nil
 				}
 
-				fmt.Fprintln(s.Err, "builtin error:", err)
+				fmt.Fprintln(shell.Err, "builtin error:", err)
 			} else {
-				s.Out = prevOut
-				s.Err = prevErr
+				shell.Out = prevOut
+				shell.Err = prevErr
 			}
 
 			if cleanup != nil {
@@ -149,15 +149,15 @@ func (s *Shell) Run() error {
 		}
 
 		//execute command
-		exitCode, err := s.executor.Execute(context.Background(), cmd, cleanArgs, ioBindings)
+		exitCode, err := shell.executor.Execute(context.Background(), command, cleanArgs, ioBindings)
 
 		if errors.Is(err, ErrNotFound) {
-			fmt.Fprintln(s.Err, cmd+": command not found")
+			fmt.Fprintln(shell.Err, command+": command not found")
 			continue
 		}
 
 		if err != nil {
-			fmt.Fprintln(s.Err, "error running command:", err)
+			fmt.Fprintln(shell.Err, "error running command:", err)
 			continue
 		}
 
@@ -171,12 +171,12 @@ func (s *Shell) Run() error {
 
 }
 
-func (s *Shell) prepareIOforRedirection(args []string) ([]string, IOBindings, func(), bool) {
+func (shell *Shell) prepareIOforRedirection(args []string) ([]string, IOBindings, func(), bool) {
 
 	ioBindings := IOBindings{
 		Stdin:  nil,
-		Stdout: s.Out,
-		Stderr: s.Err,
+		Stdout: shell.Out,
+		Stderr: shell.Err,
 	}
 
 	newArgs := make([]string, 0, len(args))
@@ -188,93 +188,31 @@ func (s *Shell) prepareIOforRedirection(args []string) ([]string, IOBindings, fu
 
 		arg := args[i]
 
-		if arg == ">" || arg == "1>" {
+		if redirection, ok := shell.redirections[arg]; ok {
+
 			if i == len(args)-1 {
-				fmt.Fprintln(s.Err, "redirect error:", ErrMissingRedirectDestination)
+				fmt.Fprintln(shell.Err, "redirect error:", ErrMissingRedirectDestination)
 				return nil, ioBindings, nil, false
 			}
 
 			dest := args[i+1]
 
-			f, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-			if err != nil {
+			file, error := os.OpenFile(dest, redirection.flag, 0644)
+			if error != nil {
 
-				fmt.Fprintf(s.Err, "open failed: %v", err)
+				fmt.Fprintf(shell.Err, "open failed: %v", error)
 				return nil, ioBindings, nil, false
 
 			}
 
-			ioBindings.Stdout = f
-			closeFuncs = append(closeFuncs, func() { f.Close() })
-			i += 2
-			continue
-
-		}
-
-		if arg == "2>" {
-			if i == len(args)-1 {
-				fmt.Fprintln(s.Err, "redirect error:", ErrMissingRedirectDestination)
-				return nil, ioBindings, nil, false
+			switch redirection.redirectType {
+			case out:
+				ioBindings.Stdout = file
+			case err:
+				ioBindings.Stderr = file
 			}
 
-			dest := args[i+1]
-
-			f, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-			if err != nil {
-
-				fmt.Fprintf(s.Err, "open failed: %v", err)
-				return nil, ioBindings, nil, false
-
-			}
-
-			ioBindings.Stderr = f
-			closeFuncs = append(closeFuncs, func() { f.Close() })
-			i += 2
-			continue
-
-		}
-
-		if arg == ">>" || arg == "1>>" {
-			if i == len(args)-1 {
-				fmt.Fprintln(s.Err, "redirect error:", ErrMissingRedirectDestination)
-				return nil, ioBindings, nil, false
-			}
-
-			dest := args[i+1]
-
-			f, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-			if err != nil {
-
-				fmt.Fprintf(s.Err, "open failed: %v", err)
-				return nil, ioBindings, nil, false
-
-			}
-
-			ioBindings.Stdout = f
-			closeFuncs = append(closeFuncs, func() { f.Close() })
-			i += 2
-			continue
-
-		}
-
-		if arg == "2>>" {
-			if i == len(args)-1 {
-				fmt.Fprintln(s.Err, "redirect error:", ErrMissingRedirectDestination)
-				return nil, ioBindings, nil, false
-			}
-
-			dest := args[i+1]
-
-			f, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-			if err != nil {
-
-				fmt.Fprintf(s.Err, "open failed: %v", err)
-				return nil, ioBindings, nil, false
-
-			}
-
-			ioBindings.Stderr = f
-			closeFuncs = append(closeFuncs, func() { f.Close() })
+			closeFuncs = append(closeFuncs, func() { file.Close() })
 			i += 2
 			continue
 
@@ -296,11 +234,11 @@ func (s *Shell) prepareIOforRedirection(args []string) ([]string, IOBindings, fu
 }
 
 // func Lookup
-func (s *Shell) Lookup(name string) (string, bool) {
+func (shell *Shell) Lookup(name string) (string, bool) {
 
-	for _, dir := range s.pathDirs {
+	for _, directory := range shell.pathDirs {
 
-		pathToCheck := filepath.Join(dir, name)
+		pathToCheck := filepath.Join(directory, name)
 
 		if info, err := os.Stat(pathToCheck); err == nil {
 			if info.Mode().IsRegular() && info.Mode()&0111 != 0 {
@@ -313,53 +251,97 @@ func (s *Shell) Lookup(name string) (string, bool) {
 
 }
 
-func (s *Shell) registerBuiltins() {
+func (shell *Shell) registerRedirections() {
 
-	s.builtins["echo"] = func(args []string, s *Shell) error {
-		fmt.Fprintln(s.Out, strings.Join(args, " "))
+	overwriteOut := Redirection{
+		redirectType: out,
+		flag:         os.O_CREATE | os.O_WRONLY | os.O_TRUNC,
+	}
+
+	overwriteErr := Redirection{
+		redirectType: err,
+		flag:         os.O_CREATE | os.O_WRONLY | os.O_TRUNC,
+	}
+
+	appendOut := Redirection{
+		redirectType: out,
+		flag:         os.O_CREATE | os.O_WRONLY | os.O_APPEND,
+	}
+
+	appendErr := Redirection{
+		redirectType: err,
+		flag:         os.O_CREATE | os.O_WRONLY | os.O_APPEND,
+	}
+
+	shell.redirections[">"] = overwriteOut
+	shell.redirections["1>"] = overwriteOut
+
+	shell.redirections["2>"] = overwriteErr
+
+	shell.redirections[">>"] = appendOut
+	shell.redirections["1>>"] = appendOut
+
+	shell.redirections["2>>"] = appendErr
+
+}
+
+// registerBuiltins initializes the shell's built-in commands map with the following commands:
+//
+// - echo: prints the arguments separated by spaces to stdout
+// - exit: terminates the shell by returning ErrExit
+// - type: displays information about a command (whether it's a builtin or external command)
+// - pwd: prints the current working directory
+// - cd: changes the current working directory, supports ~ expansion for home directory
+//
+// Each builtin is registered as a function that takes a slice of arguments and a shell pointer,
+// and returns an error if the operation fails.
+func (shell *Shell) registerBuiltins() {
+
+	shell.builtins["echo"] = func(args []string, shell *Shell) error {
+		fmt.Fprintln(shell.Out, strings.Join(args, " "))
 		return nil
 	}
 
-	s.builtins["exit"] = func(args []string, s *Shell) error {
+	shell.builtins["exit"] = func(args []string, shell *Shell) error {
 		return ErrExit
 	}
 
-	s.builtins["type"] = func(args []string, s *Shell) error {
+	shell.builtins["type"] = func(args []string, shell *Shell) error {
 
 		if len(args) == 0 {
-			fmt.Fprintln(s.Out, "type: usage: type NAME")
+			fmt.Fprintln(shell.Out, "type: usage: type NAME")
 			return nil
 		}
 
 		name := args[0]
 
 		// check builts in
-		if _, ok := s.builtins[name]; ok {
-			fmt.Fprintln(s.Out, name, "is a shell builtin")
+		if _, ok := shell.builtins[name]; ok {
+			fmt.Fprintln(shell.Out, name, "is a shell builtin")
 			return nil
 		}
 
-		if path, ok := s.Lookup(name); ok {
-			fmt.Fprintln(s.Out, name, "is", path)
+		if path, ok := shell.Lookup(name); ok {
+			fmt.Fprintln(shell.Out, name, "is", path)
 			return nil
 		}
 
-		fmt.Fprintln(s.Out, name+": not found")
+		fmt.Fprintln(shell.Out, name+": not found")
 		return nil
 	}
 
-	s.builtins["pwd"] = func(args []string, s *Shell) error {
+	shell.builtins["pwd"] = func(args []string, shell *Shell) error {
 		dir, err := os.Getwd()
 		if err == nil {
-			fmt.Fprintln(s.Out, dir)
+			fmt.Fprintln(shell.Out, dir)
 		} else {
-			fmt.Fprintln(s.Err, "error finding directory:", err)
+			fmt.Fprintln(shell.Err, "error finding directory:", err)
 		}
 
 		return nil
 	}
 
-	s.builtins["cd"] = func(args []string, s *Shell) error {
+	shell.builtins["cd"] = func(args []string, shell *Shell) error {
 
 		var target string
 
@@ -376,7 +358,7 @@ func (s *Shell) registerBuiltins() {
 		if strings.HasSuffix(target, "~") {
 			home := os.Getenv("HOME")
 			if home == "" {
-				fmt.Fprintln(s.Err, "cd: HOME not set")
+				fmt.Fprintln(shell.Err, "cd: HOME not set")
 				return nil
 			}
 
@@ -385,7 +367,7 @@ func (s *Shell) registerBuiltins() {
 			} else if strings.HasSuffix(target, "~/") {
 				target = filepath.Join(home, target[2:])
 			} else {
-				fmt.Fprintf(s.Err, "cd: unsupported user expansion: %s\n", target)
+				fmt.Fprintf(shell.Err, "cd: unsupported user expansion: %s\n", target)
 				return nil
 			}
 		}
@@ -393,11 +375,11 @@ func (s *Shell) registerBuiltins() {
 		if err := os.Chdir(target); err != nil {
 
 			if os.IsNotExist(err) {
-				fmt.Fprintf(s.Err, "cd: %s: No such file or directory\n", target)
+				fmt.Fprintf(shell.Err, "cd: %s: No such file or directory\n", target)
 			} else if os.IsPermission(err) {
-				fmt.Fprintf(s.Err, "cd: %s: Permission denied\n", target)
+				fmt.Fprintf(shell.Err, "cd: %s: Permission denied\n", target)
 			} else {
-				fmt.Fprintf(s.Err, "cd: %s: %v", target, err)
+				fmt.Fprintf(shell.Err, "cd: %s: %v", target, err)
 			}
 
 		}
